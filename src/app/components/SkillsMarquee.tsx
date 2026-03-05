05184525de369c6356f12e89d67bd4087f45602d
Ezekiel544 <chikezieezekiel3@gmail.com>
2026-03-05 14:29:06 +0000

    new feature added

--- src/app/components/SkillsMarquee.tsx
+++ src/app/components/SkillsMarquee.tsx
@@ -41,7 +41,7 @@ export function SkillsMarquee() {
               <h3 className="text-white/60 text-sm tracking-wider">{category.title}</h3>
             </div>
             
-            {/* Marquee container */}
+            {/* Marquee container added*/}
             <div className="relative overflow-hidden">
               <motion.div
                 className="flex gap-6 whitespace-nowrap"
