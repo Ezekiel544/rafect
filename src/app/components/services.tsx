bd8cee5165c5f860927e4820ebe4e538d77485fb
Ezekiel544 <chikezieezekiel3@gmail.com>
2026-03-05 14:28:43 +0000

    new feature added

--- src/app/components/services.tsx
+++ src/app/components/services.tsx
@@ -164,7 +164,7 @@ export function Services() {
           })}
         </div>
 
-        {/* Additional features section */}
+        {/* Additional features section added*/}
         <motion.div
           initial={{ opacity: 0, y: 50 }}
           animate={isInView ? { opacity: 1, y: 0 } : {}}
