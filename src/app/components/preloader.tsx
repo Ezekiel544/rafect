3f827d6e72350262313c860b57f827b5b1312972
Ezekiel544 <chikezieezekiel3@gmail.com>
2026-03-07 13:35:07 +0000

    new feature added

--- src/app/components/preloader.tsx
+++ src/app/components/preloader.tsx
@@ -43,7 +43,7 @@ export function Preloader() {
           </motion.div>
         </div>
         
-        {/* Loading text here */}
+        {/* Loading text  */}
         <motion.div
           className="text-white text-xl tracking-widest"
           initial={{ opacity: 0, y: 20 }}
