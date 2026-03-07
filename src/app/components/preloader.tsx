3dccafb319a9dcd7a82c8c7303d1139ead3619ee
Ezekiel544 <chikezieezekiel3@gmail.com>
2026-03-07 13:34:09 +0000

    new feature added

--- src/app/components/preloader.tsx
+++ src/app/components/preloader.tsx
@@ -11,7 +11,7 @@ export function Preloader() {
       <div className="flex flex-col items-center gap-16">
         {/* Rotating rings containers added now*/}
         <div className="relative w-32 h-32">
-          {/* Outer rotating rings added now*/}
+          {/* Outer rotating rings added */}
           <motion.div
             className="absolute inset-0 w-32 h-32 border-4 border-transparent border-t-white rounded-full"
             animate={{ rotate: 360 }}
