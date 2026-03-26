78f96e9f3ae54189b9c307829b5d51b0d0ffb55d
Ezekiel544 <chikezieezekiel3@gmail.com>
2026-03-26 23:05:45 +0100

    new changes made

--- src/app/components/BlogCarousel.tsx
+++ src/app/components/BlogCarousel.tsx
@@ -22,7 +22,7 @@ const blogs = [
     emoji: '🔓',
     title: 'CVSS 9.6 Account Takeover in Azerbaijan\'s Most Visited Platforms',
     category: 'Affected Company: Baku Service Platforms',
-    // description: 'How an OAuth token bypass through Open Redirect enabled complete account takeover on...',
+    description: 'How an OAuth token bypass through Open Redirect enabled complete account takeover on...',
     link: '#'
   }
 ];
