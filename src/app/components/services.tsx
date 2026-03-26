0b294f25d594a848cf62ab807a3268bca1ba4925
Ezekiel544 <chikezieezekiel3@gmail.com>
2026-03-26 15:54:39 +0100

    new changes made

--- src/app/components/services.tsx
+++ src/app/components/services.tsx
@@ -60,7 +60,7 @@ export function Services() {
     <section className="relative py-20 lg:py-32 overflow-hidden">
       {/* Background elements added*/}
       <motion.div
-        className="absolute top-1/2 left-0 w-96 h-96 bg-gray-700/20 rounded-full blur-3xl"
+        // className="absolute top-1/2 left-0 w-96 h-96 bg-gray-700/20 rounded-full blur-3xl"
         animate={{
           x: [-100, 100, -100],
           y: [-50, 50, -50],
