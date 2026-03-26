046aed16ef04b23d50221aed1c51d0ae43324d9e
Ezekiel544 <chikezieezekiel3@gmail.com>
2026-03-26 15:50:26 +0100

    new changes made

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -11,7 +11,7 @@ export function Navbar() {
       const currentScrollY = window.scrollY;
       
       if (currentScrollY < lastScrollY) {
-        // Scrolling up here now
+        // Scrolling up here now for now
         setIsVisible(true);
       } else if (currentScrollY > lastScrollY && currentScrollY > 100) {
         // Scrolling down 
