91657cbcd1192354e6f3ececce3339bb7c2d5a12
Ezekiel544 <chikezieezekiel3@gmail.com>
2026-03-26 15:53:09 +0100

    new changes made

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -18,7 +18,7 @@ export function Navbar() {
         setIsVisible(false);
       }
       
-      setLastScrollY
+      setLastScrollY(currentScrollY);
     };
 
     window.addEventListener('scroll', handleScroll, { passive: true });
