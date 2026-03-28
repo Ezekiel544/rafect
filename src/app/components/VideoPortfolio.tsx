207ac350e0ecb4ad04334d83ad1c51ee41e09b50
Ezekiel544 <chikezieezekiel3@gmail.com>
2026-03-28 14:07:40 +0100

    new changes

--- src/app/components/VideoPortfolio.tsx
+++ src/app/components/VideoPortfolio.tsx
@@ -282,9 +282,9 @@ export function VideoPortfolio() {
           <motion.div
             initial={{ opacity: 0, x: -100, rotateY: -15 }}
             animate={isInView ? { opacity: 1, x: 0, rotateY: 0 } : { opacity: 0, x: -100, rotateY: -15 }}
-            // exit={{ opacity: 0, x: 100, rotateY: 15 }}
-            // transition={{ duration: 0.6, delay: videos.length * 0.1 }}
-            // className="group relative"
+            exit={{ opacity: 0, x: 100, rotateY: 15 }}
+            transition={{ duration: 0.6, delay: videos.length * 0.1 }}
+            className="group relative"
           >
            
           </motion.div>
